d55a630dd7e3eef75554bfdd7c9b64362ba530e3
IVANCSDO <114605010+IVANCSDO@users.noreply.github.com>
2023-06-07 01:44:20 +0200

    bnoches

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+function highlightNavLinks() {
+    const sections = document.querySelectorAll("section"); // Obtener todas las secciones
+  
+    sections.forEach((section) => {
+      const navLink = document.querySelector('a[href="#' + section.id + '"]'); // Obtener el enlace de navegación correspondiente a la sección
+  
+      if (isInViewport(section)) {
+        navLink.classList.add("active"); // Agregar la clase "active" al enlace si la sección está visible
+      } else {
+        navLink.classList.remove("active"); // Quitar la clase "active" del enlace si la sección no está visible
+      }
+    });
+  }
+  
+  // Función auxiliar para verificar si un elemento está visible en el viewport
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Event Listener de scroll que llama a la función highlightNavLinks
+  window.addEventListener('scroll', highlightNavLinks);
+  
