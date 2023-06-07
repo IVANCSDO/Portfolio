ce4dde060eb0cc7438c7740312ed19bce2cd988a
IVANCSDO <114605010+IVANCSDO@users.noreply.github.com>
2023-06-07 16:04:23 +0200

    yay!

--- javascript/main.js
+++ javascript/main.js
@@ -4,10 +4,12 @@ function highlightNavLinks() {
     sections.forEach((section) => {
       const navLink = document.querySelector('a[href="#' + section.id + '"]'); // Obtener el enlace de navegación correspondiente a la sección
   
-      if (isInViewport(section)) {
-        navLink.classList.add("pri"); // Agregar la clase "active" al enlace si la sección está visible
-      } else {
-        navLink.classList.remove("pri"); // Quitar la clase "active" del enlace si la sección no está visible
+      if(navLink){
+        if (isInViewport(section)) {
+          navLink.classList.add("pri"); // Agregar la clase "active" al enlace si la sección está visible
+        } else {
+          navLink.classList.remove("pri"); // Quitar la clase "active" del enlace si la sección no está visible
+        }
       }
     });
   }
@@ -25,4 +27,40 @@ function highlightNavLinks() {
   
   // Event Listener de scroll que llama a la función highlightNavLinks
   window.addEventListener('scroll', highlightNavLinks);
+  
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a[href^="#"]');
+    
+    console.log(links)
+
+    links.forEach(function(link, index) {
+      link.addEventListener('click', function(e) {
+        if (index === 0) {
+          // Comportamiento predeterminado para el primer enlace
+          return
+        }
+
+        e.preventDefault();
+        
+        var targetId = this.getAttribute('href').substring(1);
+        var targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+          var offsetTop = targetElement.offsetTop;
+          var windowHeight = window.innerHeight;
+          var scrollToPos = offsetTop - (windowHeight / 4);
+          
+          window.scrollTo({
+            top: scrollToPos,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+
+    
+
+  });
   
